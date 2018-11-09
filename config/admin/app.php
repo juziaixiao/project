ec967dbd7da26cc4b2ea4c80c3becdad82f07b57
juziaixiao <3431274990@qq.com>
2018-11-09 13:10:45 +0800

    config folder

---
+++ config/admin/app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @Projectname
+ * @Author 鞠慧宇
+ * @Date 2018/10/18
+ * @Email:juziaixiao@chunyimail.com
+ * @Context  admin模块配置文件
+ */
+//return [
+//    'default_return_type'    => 'html',
+//    // 默认跳转页面对应的模板文件
+//    'dispatch_success_tmpl'  => APP_PATH . 'admin' . DS . 'view/common/200.html',
+//    'dispatch_error_tmpl'    => APP_PATH . 'admin' . DS . 'view/common/404.html',
+//
+//    'app_debug'              =>true,
+//];
---
+++ config/api/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Kenneth Luff.
+ * Author: Kenneth Luff
+ * Email: kennethluff@outlook.com
+ */
+//配置文件
+//return [
+//    'setting'=>[
+//        'img_prefix'=>'/'
+//    ]
+//];
